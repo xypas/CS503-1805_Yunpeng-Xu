f771206e6be437d329aab2e2d96e68600d6b5caf
xypas <yunpengx@usc.edu>
2018-10-09 17:31:11 -0700

    week1 optional navbar

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { ProblemDetailComponent } from './components/problem-detail/problem-deta
 
 import { routing } from './app.routes';
 import { NewProblemComponent } from './components/new-problem/new-problem.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProblemListComponent,
     ProblemDetailComponent,
-    NewProblemComponent
+    NewProblemComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
